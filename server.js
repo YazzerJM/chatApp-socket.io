7dcd568635bb7609f323e8b9266c555026f2659a
Yasser Jimenez <yasser.jimenez@segro.edu.mx>
2021-11-16 11:26:10 -0600

    ejemplo terminado

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ const io = require('socket.io')(server, {
 
 io.on('connection', (socket) => {
     console.log('connection');
+    socket.on('sendChatToServer', (message) => {
+        console.log(message);
 
+        // io.sockets.emit('sendChatToClient', message);
+        socket.broadcast.emit('sendChatToClient', message);
+    });
     socket.on('disconnect', (socket) => {
         console.log('Disconnect');
     });
